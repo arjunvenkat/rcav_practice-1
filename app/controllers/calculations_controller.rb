75f4e076d492fc99ca9b4e491ff0c07e53aa3cdd
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-06 14:15:24 -0500

    finishes first two problems

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,7 +2,14 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+    number = params["number"].to_i
+    @result = number ** 2
+  end
+
   def sqrt
+    number = params["number"].to_i
+    @result = Math.sqrt(number)
   end
 
   def pmt
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:number/", { :controller => "calculations", :action => "square"})
+  get("/square_root/:number", { :controller => "calculations", :action => "sqrt"})
 end
